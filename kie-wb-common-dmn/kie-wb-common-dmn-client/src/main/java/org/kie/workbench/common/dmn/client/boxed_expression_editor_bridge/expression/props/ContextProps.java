95c3b0c9251f48123df5ea235ef93da73c10be80
vpellegrino <vpellegr@redhat.com>
2021-03-11 17:17:44 +0100

    Introducing context js interop props

---
+++ kie-wb-common-dmn/kie-wb-common-dmn-client/src/main/java/org/kie/workbench/common/dmn/client/boxed_expression_editor_bridge/expression/props/ContextProps.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2021 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.kie.workbench.common.dmn.client.boxed_expression_editor_bridge.expression.props;
+
+import jsinterop.annotations.JsType;
+
+@JsType
+public class ContextProps extends ExpressionProps {
+    public final ContextEntryProps[] contextEntries;
+    public final ExpressionProps result;
+
+    public ContextProps(final String name, final String dataType, final ContextEntryProps[] contextEntries, final ExpressionProps result) {
+        super(name, dataType, "Context");
+        this.contextEntries = contextEntries;
+        this.result = result;
+    }
+}
