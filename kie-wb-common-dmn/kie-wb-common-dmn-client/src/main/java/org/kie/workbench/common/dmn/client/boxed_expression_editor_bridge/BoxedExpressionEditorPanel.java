26e94087e7fd9ddc1ebac3ad80fe675e0949db99
vpellegrino <vpellegr@redhat.com>
2021-03-11 17:20:37 +0100

    Using of ExpressionFiller for populating both expression and js interop classes as needed

--- kie-wb-common-dmn/kie-wb-common-dmn-client/src/main/java/org/kie/workbench/common/dmn/client/boxed_expression_editor_bridge/BoxedExpressionEditorPanel.java
+++ kie-wb-common-dmn/kie-wb-common-dmn-client/src/main/java/org/kie/workbench/common/dmn/client/boxed_expression_editor_bridge/BoxedExpressionEditorPanel.java
@@ -16,11 +16,6 @@
 
 package org.kie.workbench.common.dmn.client.boxed_expression_editor_bridge;
 
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.event.Event;
 
@@ -35,22 +30,16 @@ import org.kie.workbench.common.dmn.api.definition.HasExpression;
 import org.kie.workbench.common.dmn.api.definition.HasName;
 import org.kie.workbench.common.dmn.api.definition.HasVariable;
 import org.kie.workbench.common.dmn.api.definition.model.Context;
-import org.kie.workbench.common.dmn.api.definition.model.DecisionTable;
 import org.kie.workbench.common.dmn.api.definition.model.Expression;
-import org.kie.workbench.common.dmn.api.definition.model.FunctionDefinition;
-import org.kie.workbench.common.dmn.api.definition.model.InformationItem;
 import org.kie.workbench.common.dmn.api.definition.model.InformationItemPrimary;
-import org.kie.workbench.common.dmn.api.definition.model.Invocation;
-import org.kie.workbench.common.dmn.api.definition.model.IsLiteralExpression;
-import org.kie.workbench.common.dmn.api.definition.model.List;
 import org.kie.workbench.common.dmn.api.definition.model.LiteralExpression;
 import org.kie.workbench.common.dmn.api.definition.model.Relation;
 import org.kie.workbench.common.dmn.api.editors.types.BuiltInTypeUtils;
 import org.kie.workbench.common.dmn.api.property.dmn.Name;
 import org.kie.workbench.common.dmn.api.property.dmn.QName;
-import org.kie.workbench.common.dmn.api.property.dmn.Text;
 import org.kie.workbench.common.dmn.api.property.dmn.types.BuiltInType;
-import org.kie.workbench.common.dmn.client.boxed_expression_editor_bridge.expression.props.Column;
+import org.kie.workbench.common.dmn.client.boxed_expression_editor_bridge.expression.converters.ExpressionFiller;
+import org.kie.workbench.common.dmn.client.boxed_expression_editor_bridge.expression.props.ContextProps;
 import org.kie.workbench.common.dmn.client.boxed_expression_editor_bridge.expression.props.ExpressionProps;
 import org.kie.workbench.common.dmn.client.boxed_expression_editor_bridge.expression.props.LiteralExpressionProps;
 import org.kie.workbench.common.dmn.client.boxed_expression_editor_bridge.expression.props.RelationProps;
@@ -107,28 +96,8 @@ public class BoxedExpressionEditorPanel extends Composite {
             final HasVariable<InformationItemPrimary> hasVariable = (HasVariable<InformationItemPrimary>) hasExpression;
             dataType = hasVariable.getVariable().getTypeRef().getLocalPart();
         }
-        ExpressionProps expressionProps = new ExpressionProps(expressionName, dataType, null);
-        if (hasExpression != null && hasExpression.getExpression() != null) {
-            final Expression wrappedExpression = hasExpression.getExpression();
-            if (wrappedExpression instanceof IsLiteralExpression) {
-                final LiteralExpression literalExpression = (LiteralExpression) wrappedExpression;
-                expressionProps = new LiteralExpressionProps(expressionName, dataType, literalExpression.getText().getValue());
-            } else if (wrappedExpression instanceof Context) {
-
-            } else if (wrappedExpression instanceof Relation) {
-                final Relation relationExpression = (Relation) wrappedExpression;
-                expressionProps = new RelationProps(expressionName, dataType, columnsConvertForRelationProps(relationExpression), rowsConvertForRelationProps(relationExpression));
-            } else if (wrappedExpression instanceof List) {
-
-            } else if (wrappedExpression instanceof Invocation) {
-
-            } else if (wrappedExpression instanceof FunctionDefinition) {
-
-            } else if (wrappedExpression instanceof DecisionTable) {
-
-            }
-        }
-        BoxedExpressionService.renderBoxedExpressionEditor(containerId, expressionProps);
+        final Expression expression = hasExpression != null && hasExpression.getExpression() != null ? hasExpression.getExpression() : null;
+        BoxedExpressionService.renderBoxedExpressionEditor(containerId, ExpressionFiller.buildAndFillJsInteropProp(expression, expressionName, dataType));
         BoxedExpressionService.registerBroadcastForExpression(this);
     }
 
@@ -142,10 +111,38 @@ public class BoxedExpressionEditorPanel extends Composite {
     }
 
     public void broadcastLiteralExpressionDefinition(final LiteralExpressionProps literalExpressionProps) {
+        setExpressionName(literalExpressionProps);
+        setTypeRef(literalExpressionProps.dataType);
+        if (hasExpression.getExpression() == null) {
+            hasExpression.setExpression(new LiteralExpression());
+        }
+        ExpressionFiller.fillLiteralExpression((LiteralExpression) hasExpression.getExpression(), literalExpressionProps);
+    }
+
+    public void broadcastContextExpressionDefinition(final ContextProps contextProps) {
+        setExpressionName(contextProps);
+        setTypeRef(contextProps.dataType);
+        if (hasExpression.getExpression() == null) {
+            hasExpression.setExpression(new Context());
+        }
+        ExpressionFiller.fillContextExpression((Context) hasExpression.getExpression(), contextProps);
+    }
+
+    public void broadcastRelationExpressionDefinition(final RelationProps relationProps) {
+        if (hasExpression.getExpression() == null) {
+            hasExpression.setExpression(new Relation());
+        }
+        ExpressionFiller.fillRelationExpression((Relation) hasExpression.getExpression(), relationProps);
+    }
+
+    private void setExpressionName(final ExpressionProps expressionProps) {
         final HasName hasName = (HasName) hasExpression;
-        hasName.setName(new Name(literalExpressionProps.name));
+        hasName.setName(new Name(expressionProps.name));
+    }
+
+    private void setTypeRef(final String dataType) {
         final QName typeRef = BuiltInTypeUtils
-                .findBuiltInTypeByName(literalExpressionProps.dataType)
+                .findBuiltInTypeByName(dataType)
                 .orElse(BuiltInType.UNDEFINED)
                 .asQName();
         if (hasExpression instanceof HasVariable) {
@@ -153,76 +150,5 @@ public class BoxedExpressionEditorPanel extends Composite {
             HasVariable<InformationItemPrimary> hasVariable = (HasVariable<InformationItemPrimary>) hasExpression;
             hasVariable.getVariable().setTypeRef(typeRef);
         }
-        if (hasExpression.getExpression() == null) {
-            hasExpression.setExpression(new LiteralExpression());
-        }
-        final LiteralExpression literalExpression = (LiteralExpression) hasExpression.getExpression();
-        literalExpression.setText(new Text(literalExpressionProps.content));
-        literalExpression.setTypeRef(typeRef);
-    }
-
-    public void broadcastRelationExpressionDefinition(final RelationProps relationProps) {
-        if (hasExpression.getExpression() == null) {
-            hasExpression.setExpression(new Relation());
-        }
-        final Relation relationExpression = (Relation) hasExpression.getExpression();
-        relationExpression.getColumn().clear();
-        relationExpression.getColumn().addAll(columnsConvertForRelationExpression(relationProps));
-        relationExpression.getRow().clear();
-        relationExpression.getRow().addAll(rowsConvertForRelationExpression(relationProps, relationExpression));
-    }
-
-    private Collection<List> rowsConvertForRelationExpression(final RelationProps relationProps, final Relation relationExpression) {
-        return Arrays
-                .stream(relationProps.rows)
-                .map(row -> {
-                    final List list = new List();
-                    list.getExpression().addAll(
-                            Arrays.stream(row).map(cell -> {
-                                final LiteralExpression wrappedExpression = new LiteralExpression();
-                                wrappedExpression.setText(new Text(cell));
-                                wrappedExpression.setTypeRef(BuiltInType.STRING.asQName());
-                                return HasExpression.wrap(relationExpression, wrappedExpression);
-                            }).collect(Collectors.toList())
-                    );
-                    return list;
-                })
-                .collect(Collectors.toList());
-    }
-
-    private Collection<InformationItem> columnsConvertForRelationExpression(final RelationProps relationProps) {
-        return Stream
-                .of(relationProps.columns)
-                .map(column -> {
-                    final InformationItem informationItem = new InformationItem();
-                    informationItem.setName(new Name(column.name));
-                    informationItem.setTypeRef(BuiltInTypeUtils
-                                                       .findBuiltInTypeByName(column.dataType)
-                                                       .orElse(BuiltInType.UNDEFINED)
-                                                       .asQName());
-                    return informationItem;
-                })
-                .collect(Collectors.toList());
-    }
-
-    private Column[] columnsConvertForRelationProps(final Relation relationExpression) {
-        return relationExpression
-                .getColumn()
-                .stream()
-                .map(informationItem -> new Column(informationItem.getName().getValue(), informationItem.getTypeRef().getLocalPart(), null))
-                .toArray(Column[]::new);
-    }
-
-    private String[][] rowsConvertForRelationProps(final Relation relationExpression) {
-        return relationExpression
-                .getRow()
-                .stream()
-                .map(list -> list
-                        .getExpression()
-                        .stream()
-                        .map(wrappedLiteralExpression -> ((LiteralExpression) wrappedLiteralExpression.getExpression()).getText().getValue())
-                        .toArray(String[]::new)
-                )
-                .toArray(String[][]::new);
     }
 }
